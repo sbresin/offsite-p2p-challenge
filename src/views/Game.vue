4a7bb51574dec354c9ec4e883e6fdc671acb5669
Ali Jahanpak <a.jahanpak@gmail.com>
2024-11-22 13:43:07 +0100

    Add store and update components

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { usePlayer } from '../stores/player.ts';
 
-const player = ref('Player')
+const player = usePlayer(); // Access the Pinia store
 </script>
 
 <template>
-  <p>Hello {{ player }}</p>
+  <p>Hello {{ player.name }}</p>
 </template>
 
 <style scoped>
