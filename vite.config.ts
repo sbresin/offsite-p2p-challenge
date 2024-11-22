42037fc7b02e74aab1c68990cc59ca080b41e6b3
ecirsulga <ecir.sulga@thermondo.de>
2024-11-22 11:08:40 +0100

    gundb added to dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+
+const moduleExclude = match => {
+  const m = id => id.indexOf(match) > -1
+  return {
+    name: `exclude-${match}`,
+    resolveId(id) {
+      if (m(id)) return id
+    },
+    load(id) {
+      if (m(id)) return `export default {}`
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [vue()],
+  optimizeDeps: {
+    include: [
+      'gun',
+      'gun/gun',
+      'gun/sea',
+      'gun/sea.js',
+      'gun/lib/then',
+      'gun/lib/webrtc',
+      'gun/lib/radix',
+      'gun/lib/radisk',
+      'gun/lib/store',
+      'gun/lib/rindexed',
+    ],
+  },
+  plugins: [
+    moduleExclude('text-encoding'),
+    vue(),
+  ],
 })
